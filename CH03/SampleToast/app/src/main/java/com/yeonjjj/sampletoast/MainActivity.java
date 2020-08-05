e3a12bfd7684642b8c7e918643cb293448d44f74
YYJ <hidua1124@naver.com>
2020-08-05 22:14:54 +0900

    3.4. 토스트, 스낵바 그리고 대화상자 사용하기(1)
    - 토스트 사용하기
    - 스택바 사용하기

---
+++ CH03/SampleToast/app/src/main/java/com/yeonjjj/sampletoast/MainActivity.java
@@ -0,0 +1,62 @@
+package com.yeonjjj.sampletoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+    }
+
+    public void onButton1Clicked(View v){
+        try {
+            Toast toastView = Toast.makeText(this, "위치가 바뀐 토스트 메시지입니다.", Toast.LENGTH_LONG);
+            int xOffset = Integer.parseInt(editText.getText().toString());
+            int yOffset = Integer.parseInt(editText2.getText().toString());
+
+            toastView.setGravity(Gravity.TOP|Gravity.LEFT, xOffset, yOffset);
+            toastView.show();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onButton2Clicked(View v){
+        LayoutInflater inflater = getLayoutInflater();      // 레이아웃 인플레이터 객체 참조
+
+        View layout = inflater.inflate(R.layout.toastboarder, (ViewGroup) findViewById(R.id.toast_layout_root));
+
+        TextView text = layout.findViewById(R.id.text);
+
+        Toast toast = new Toast(this);
+        text.setText("모양 바꾼 토스트");
+        toast.setGravity(Gravity.CENTER, 0, -100);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public void onButton3Clicked(View v){
+        Snackbar.make(v, "스낵바입니다.", Snackbar.LENGTH_LONG).show();
+    }
+
+}
