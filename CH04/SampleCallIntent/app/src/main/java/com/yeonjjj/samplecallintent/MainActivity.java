795898654f7205830eae26b6e3a4c22a778540a9
YYJ <hidua1124@naver.com>
2020-08-06 01:06:24 +0900

    4.3. 인텐트 살펴보기

---
+++ CH04/SampleCallIntent/app/src/main/java/com/yeonjjj/samplecallintent/MainActivity.java
@@ -0,0 +1,49 @@
+package com.yeonjjj.samplecallintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = editText.getText().toString();    // 입력상자에 입력된 전화번호 확인
+
+                // 전화걸기 화면을 보여줄 인텐트 객체 생성
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                startActivity(intent);      // 액티비티 띄우기
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                ComponentName name = new ComponentName(      // 컴포넌트 이름을 지정할 수 있는 객체 생성
+                        "com.yeonjjj.samplecallintent", "com.yeonjjj.samplecallintent.MenuActivity");
+                intent.setComponent(name);      // 인텐트 객체에 컴포넌트 지정
+                startActivityForResult(intent, 101);    // 액티비티 띄우기
+            }
+        });
+
+    }
+}
